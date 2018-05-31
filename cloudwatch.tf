d2a2f4bb223e086d4e0698a000aac62528b5104d
zhengzhoumin <zhengzhoumin@mobifun365.net>
2018-05-31 21:09:24 +0800

    fix bug

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -24,28 +24,28 @@ resource "aws_cloudwatch_event_target" "lambda" {
   target_id = "${var.function_name}"
   rule      = "${aws_cloudwatch_event_rule.lambda.name}"
   arn       = "${aws_lambda_function.lambda.arn}"
-  count     = "${! var.attach_vpc_config && ! var.attach_dead_letter_config ? 1 : 0}"
+  count     = "${local.schedule_enabled && ! var.attach_vpc_config && ! var.attach_dead_letter_config ? 1 : 0}"
 }
 
 resource "aws_cloudwatch_event_target" "lambda_with_dl" {
   target_id = "${var.function_name}"
   rule      = "${aws_cloudwatch_event_rule.lambda.name}"
   arn       = "${aws_lambda_function.lambda_with_dl.arn}"
-  count     = "${var.attach_dead_letter_config && ! var.attach_vpc_config ? 1 : 0}"
+  count     = "${local.schedule_enabled && var.attach_dead_letter_config && ! var.attach_vpc_config ? 1 : 0}"
 }
 
 resource "aws_cloudwatch_event_target" "lambda_with_vpc" {
   target_id = "${var.function_name}"
   rule      = "${aws_cloudwatch_event_rule.lambda.name}"
   arn       = "${aws_lambda_function.lambda_with_vpc.arn}"
-  count     = "${var.attach_vpc_config && ! var.attach_dead_letter_config ? 1 : 0}"
+  count     = "${local.schedule_enabled && var.attach_vpc_config && ! var.attach_dead_letter_config ? 1 : 0}"
 }
 
 resource "aws_cloudwatch_event_target" "lambda_with_dl_and_vpc" {
   target_id = "${var.function_name}"
   rule      = "${aws_cloudwatch_event_rule.lambda.name}"
   arn       = "${aws_lambda_function.lambda_with_dl_and_vpc.arn}"
-  count     = "${var.attach_dead_letter_config && var.attach_vpc_config ? 1 : 0}"
+  count     = "${local.schedule_enabled && var.attach_dead_letter_config && var.attach_vpc_config ? 1 : 0}"
 }
 
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
